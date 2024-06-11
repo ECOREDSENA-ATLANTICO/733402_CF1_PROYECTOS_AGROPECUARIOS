b5424eb62ce8d1b711e4d70bd9ec6a219cf0c04b
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2024-06-11 11:52:00 -0500

    up

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,227 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Sistema Nacional de Innovación Agropecuaria (SNIA) 
+
+    .row.justify-content-center
+      p La Ley 1876 de diciembre 27 de 2017 Sistema Nacional de Innovación Agropecuaria se crea con el fin de dar respuesta al primer punto del acuerdo de paz y derogar la Ley 607 de 2000 que creó las Unidad Municipal de Asistencia Técnica Agropecuaria (UMATA) y reglamentó la asistencia técnica directa rural; en esta normativa se establecen los criterios para la creación, articulación y puesta en marcha de tres subsistemas, además, de su respectiva reglamentación mediante decretos, resoluciones y ordenanzas.
+
+
+    .row.justify-content-center.align-items-center
+      .col-lg-8
+
+        .img-fnd-flot3.mb-3
+          img.b-img3(src='@/assets/curso/temas/tema2/img-1.svg' alt='Background Image')
+          .fl-box3
+            p.mb-0 A continuación se brindará una explicación breve de la jerarquía de las normas, la aplicación de la Ley 1876 de 2017 (SNIA) y normas reglamentarias para la formulación de proyectos agropecuarios.
+      
+    Separador
+    #2_1.titulo-segundo.color-acento-contenido
+      h2 2.1 Jerarquía de las normas en Colombia
+
+    p.mb-4 En el estado colombiano las normas se encuentran jerarquizadas, “no tienen todas el mismo nivel, ni la misma categoría, ni la misma importancia. Se encuentran jerarquizadas, según su mayor o menor grado de generalidad en consecuencia las normas superiores pueden modificar o derogar válidamente las normas inferiores y éstas a su vez deben respetar el contenido de las normas superiores.” 
+
+
+    .row.justify-content-center
+      .col-lg-3
+        .crd_03.mb-5
+          .crd_03__icon
+            img(src="@/assets/curso/temas/tema2/img-2.svg", alt="alt")          
+          .crd_03__txt
+            p <b>Norma constitucional:</b> ocupa dentro del ordenamiento jurídico el rango más elevado, es la norma suprema dentro del ordenamiento jurídico.
+      .col-lg-4
+        .crd_03.mb-5
+          .crd_03__icon
+            img(src="@/assets/curso/temas/tema2/img-2.svg", alt="alt")          
+          .crd_03__txt.py-4
+            p <b>Ley: </b>norma general y abstracta que en su creación debe tener en cuenta la norma constitucional y que es creada según los preceptos establecidos en las normas constitucionales.
+      .col-lg-5
+        .crd_03.mb-5
+          .crd_03__icon
+            img(src="@/assets/curso/temas/tema2/img-2.svg", alt="alt")          
+          .crd_03__txt
+            p <b>Reglamento:</b> norma jurídica que busca hacer posible que la ley se cumpla para que no quede en el papel, y detallar las leyes en su contenido para adaptarlas al medio económico, político y social que regula. Ejemplo: decretos, resoluciones, ordenanzas, acuerdos, sentencias, actos jurídicos de derecho privado, etc. Sierra, 1988.
+ 
+
+    .row.justify-content-center.align-items-center
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 1.
+          span Jerarquía de las normas en Colombia.
+        .row.justify-content-center.align-items-center
+          .col-lg-12
+            figure.mb-5
+              img(src='@/assets/curso/temas/tema2/img-3.png', alt='')
+              figcaption Nota. Elaboración propia. Normatividad del sector agropecuario en Colombia.
+
+
+    Separador
+    #2_2.titulo-segundo.color-acento-contenido
+      h2 2.2 Sistema Nacional de Innovación Agropecuaria Ley 1876 de diciembre 27 de 2017
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6    
+        figure
+          img(src='@/assets/curso/temas/tema2/img-4.png', alt='')         
+      .col-lg-6      
+        p.mb-4 La Ley 1876 de diciembre 27 de 2017 del Sistema Nacional de Innovación Agropecuaria, se establece con el objeto de crear y poner en marcha el Sistema Nacional de Innovación Agropecuaria (SNIA), compuesto por subsistemas, planes estratégicos, instrumentos de planificación y participación, plataformas de gestión, procedimientos para su implementación, así como mecanismos para su financiación, seguimiento y evaluación; además crea nuevas funciones, competencias y mecanismos de articulación de las entidades y organismos de coordinación del orden nacional y territorial que componen el SNIA y crea el servicio público de extensión agropecuaria y normas para su prestación (Diciembre 27 de 2017 DO N° 50.461).
+        p La Ley 1876 de 2017 se crea en el artículo 4 del SNIA, como un subsistema del Sistema Nacional de Competitividad, Ciencia, Tecnología e Innovación (SNCCT), que será coordinado por el Ministerio de Agricultura y Desarrollo Rural (MADR); el SNIA está integrado por tres subsistemas:
+        .row.justify-content-center.align-items-center.mb-5
+          .row.tt.mb-2
+            .col-2
+              h3.mb-0 01	
+            .col-10              
+              p.mb-0 Subsistema nacional de investigación y desarrollo tecnológico agropecuario
+          .row.tt.mb-2
+            .col-2
+              h3.mb-0 02	
+            .col-10              
+              p.mb-0 Subsistema nacional de extensión agropecuaria
+          .row.tt
+            .col-2
+              h3.mb-0 03	
+            .col-10              
+              p.mb-0 Subsistema Nacional de formación y capacitación para la innovación agropecuaria
+
+
+    .row.justify-content-center.align-items-center
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 2.
+          span Estructura del SNIA
+        .row.justify-content-center.align-items-center
+          .col-lg-12
+            figure.mb-5
+              img(src='@/assets/curso/temas/tema2/img-5.png', alt='')
+              figcaption Nota. Elaboración propia
+
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6
+        p En la formulación de proyectos productivos agropecuarios se debe tener en cuenta las orientaciones que en materia de normatividad existente en el país, la Ley 1876 de 2017 Diciembre 27 de 2017 DO N° 50.461 es el marco normativo que guía la política agraria en Colombia; en el Título III, artículo 24 define la extensión agropecuaria como un bien y un servicio de carácter público, permanente y descentralizado; y comprende las acciones de acompañamiento integral orientadas a diagnosticar, recomendar, actualizar, capacitar, transferir, asistir, empoderar y generar competencias en los productores agropecuarios para que estos incorporen en su actividad productiva prácticas, productos tecnológicos, tecnologías, conocimientos y comportamientos que beneficien su desempeño y mejoren su competitividad y sostenibilidad, así como su aporte a la seguridad alimentaria y su desarrollo como ser humano integral.
+
+        p La norma establece cinco aspectos que se deben tener en cuenta en el momento de prestar el servicio de extensión agropecuaria, como son:
+
+      .col-lg-6
+        figure.mb-5
+          img(src='@/assets/curso/temas/tema2/img-6.png', alt='')
+
+    .row.justify-content-center.d-flex.align-items-stretch.mb-5
+    
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/tema2/img-7.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones.fnd-5
+            .p-4
+              h6.text-center Desarrollo de capacidades humanas integrales.
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/tema2/img-8.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones.fnd-5
+            .p-4
+              h6.text-center Desarrollo de capacidades sociales integrales y fortalecimiento a la asociatividad.
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/tema2/img-9.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones.fnd-5
+            .p-4
+              h6.text-center Acceso y aprovechamiento efectivo de la información de apoyo de las tecnologías y productos tecnológicos.
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/tema2/img-10.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones.fnd-5
+            .p-4
+              h6.text-center Gestión sostenible de los recursos naturales.
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar
+          img(src='@/assets/curso/temas/tema2/img-11.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones.fnd-5
+            .p-3
+              h6.text-center Desarrollo de habilidades para la participación de los productores en espacios de política pública.
+    
+    p Al mismo tiempo estos aspectos hacen parte de la herramienta de registro y clasificación de usuarios en la que se debe registrar a los productores para poder ser beneficiarios de los proyectos agropecuarios que sean gestionados en el municipio.
+
+    .bloque-texto-g.color-acento-contenido.p-3.p-sm-4.p-md-5.mb-5
+        .bloque-texto-g__img(
+          :style="{'background-image': `url(${require('@/assets/curso/temas/tema2/img-12.jpg')})`}"
+        )
+        .bloque-texto-g__texto.p-4
+          p.mb-0 En el proceso de extensión agropecuaria los profesionales del sector deben realizar el acompañamiento a productores según el grado de organización, si son población dispersa pero definida por un territorio (vereda) o línea productiva (ganadera, agrícola, agroindustrial); si hacen parte de una organización comunitaria (asociación, cooperativa, etc.) o si tienen una empresa agropecuaria rural. En este acompañamiento el extensionista fortalece, capacita y acompaña a la organización en temas normativos para ello debe tener en cuenta el PDEA que en el artículo 29 lo define como el Plan departamental de extensión agropecuaria el cual es el instrumento de planificación cuatrienal en cada departamento, que en coordinación con sus municipios, distritos y demás actores del SNIA, definirá los elementos estratégicos y operativos para la prestación del servicio de extensión. En este mismo artículo 29 se hace referencia a los elementos mínimos que debe tener el PDEA (Congreso de Colombia, 2017):
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-4
+        figure.mb-5
+          img(src='@/assets/curso/temas/tema2/img-13.png', alt='')
+      .col-lg-8
+
+        SlyderF.mb-5(columnas="col-lg-6 col-xl-6")
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-1
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center 
+            p.text-center Líneas productivas priorizadas a atender mediante los servicios de extensión agropecuaria, sus limitantes y requerimientos.
+
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-2
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center 
+            p.text-center Población objeto del servicio caracterizada respecto a sus condiciones socioeconómicas, culturales y productivas.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-3
+            .row.justify-content-center.mb-3
+
+            h2.text-center 
+            p.text-center.px-3 Estrategias y actividades requeridas para dar solución a las problemáticas y/o potenciar los sistemas productivos, el capital social, el capital humano, y la gestión de los recursos naturales, con sus respectivos cronogramas de ejecución.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center 
+            p.text-center Los objetivos, indicadores y metas en términos de productividad, competitividad y generación de ingresos.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-5
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center 
+            p.text-center La planificación financiera y de gastos asociados a la prestación del servicio.
+
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-6
+            .row.justify-content-center.mb-3
+
+            h2.text-center 
+            p.text-center Programas y proyectos regionales para la generación de capacidades, acceso a mercados y provisión de bienes, servicios e infraestructura sectorial, con los cuales deba articularse el servicio de extensión agropecuaria.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-7
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center 
+            p.text-center Las acciones regionales de manejo sostenible de los recursos naturales, de gestión del riesgo agroclimático y de adaptación al cambio climático.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-8
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center
+            p.text-center Los sistemas territoriales de innovación, alianzas interinstitucionales, redes e iniciativas orientadas a la innovación agropecuaria regional, que deban ser articuladas con el servicio público de extensión agropecuaria.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-9
+            .row.justify-content-center.mb-3
+
+            h2.text-center 
+            p.text-center La articulación con los planes y programas de prestación de servicios de extensión o asistencia técnica agropecuaria adelantados con recursos de los Fondos Parafiscales Agropecuarios y Pesqueros. En el caso que el PDEA incluya productores ya beneficiados por la parafiscalidad agropecuaria, en la definición de la tarifa del servicio no se tendrá en cuenta el aporte correspondiente.
+
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-10
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center
+            p.text-center Programas y proyectos para mejorar y/o mantener el estatus sanitario, fitosanitario y de inocuidad en la producción agropecuaria.
+          .tarjeta.color-acento-botones.p-4.fnd-6.fnd-6-11
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/temas/tema2/img-r.svg' alt='AvatarTop')
+            h2.text-center
+            p.text-center Desarrollo y uso de nuevas tecnologías para la información y la comunicación TIC para impulsar, apoyar y/o soportar los distintos procesos de gestión de conocimiento que hacen parte de la extensión agropecuaria.
+          
 
 </template>
 
