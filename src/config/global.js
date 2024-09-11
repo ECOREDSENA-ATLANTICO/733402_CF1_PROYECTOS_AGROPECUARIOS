5e492a3a9a5b7b8babdbfd8eaec21e4ca35ed203
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2024-09-11 15:17:07 -0500

    up

--- src/config/global.js
+++ src/config/global.js
@@ -127,7 +127,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/733402_CFA01_DU.pdf',
+        download: 'downloads/733402_CF01_CFA.pdf',
       },
       {
         icono: 'fas fa-download',
